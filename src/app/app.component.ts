eee4d2b08738effdc3e2acd81b22177bbf78d889
srkreddydendi <srkreddydendi@gmail.com>
2020-01-16 18:15:20 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { FormControl, FormBuilder, FormGroup, FormArray } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular';
   myForm: FormGroup;
   arr: FormArray;
@@ -15,21 +15,23 @@ export class AppComponent  {
 
   ngOnInit() {
     this.myForm = this.fb.group({
-      arr: this.fb.array([this.createItem()])
+      arr: this.fb.array([])
     })
+    const childArr = this.myForm.controls.arr as FormArray;
+    childArr.push(this.createItem());
   }
 
   createItem() {
     return this.fb.group({
-      name: [''],
-      age: [''],
+      name: ['abc'],
+      age: ['21'],
       address: this.fb.array([
-        {city:[''],
-        state:['']
-        },
-        {city:[''],
-        state:['']
-        }
+        this.fb.group({city:[''],
+        state:['state']
+        }),
+        this.fb.group({city:[''],
+        state:['state1']
+        })
       ])
     })
   }
